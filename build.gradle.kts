905ec46dbce29e959d8b6c5364e47fc46b5a7fdf
Koji Suehiro <tiripeppa@gmail.com>
2024-10-27 20:51:07 +0900

    build.gradle修正

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
-	id 'org.jetbrains.kotlin.jvm' version '1.9.25'
-	id 'org.jetbrains.kotlin.plugin.spring' version '1.9.25'
-	id 'org.springframework.boot' version '3.3.5'
-	id 'io.spring.dependency-management' version '1.1.6'
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
-group = 'com.example'
-version = '0.0.1-SNAPSHOT'
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
@@ -19,19 +20,18 @@ repositories {
 }
 
 dependencies {
-	implementation 'org.springframework.boot:spring-boot-starter'
-	implementation 'org.jetbrains.kotlin:kotlin-reflect'
-	testImplementation 'org.springframework.boot:spring-boot-starter-test'
-	testImplementation 'org.jetbrains.kotlin:kotlin-test-junit5'
-	testRuntimeOnly 'org.junit.platform:junit-platform-launcher'
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    runtimeOnly("com.h2database:h2") // 必要に応じて他のDBに変更
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll '-Xjsr305=strict'
-	}
+    jvmToolchain(21)
 }
 
-tasks.named('test') {
-	useJUnitPlatform()
+tasks.test {
+    useJUnitPlatform()
 }
