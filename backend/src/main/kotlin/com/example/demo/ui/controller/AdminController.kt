a6238e16f672238c098a9035909a9dd435ee39cb
Koji Suehiro <tiripeppa@gmail.com>
2024-10-31 22:39:02 +0900

    フロントエンドコンテナのCORSの許可設定追加

--- backend/src/main/kotlin/com/example/demo/ui/controller/AdminController.kt
+++ backend/src/main/kotlin/com/example/demo/ui/controller/AdminController.kt
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/admin")
 class AdminController(
     private val categoryService: CategoryService,
     private val itemService: ItemService
