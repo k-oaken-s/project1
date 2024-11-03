139e6d4462c59999578fe1809dcc9f218a22a584
Koji Suehiro <tiripeppa@gmail.com>
2024-11-03 22:02:59 +0900

    git ignore bin

--- backend/bin/main/com/example/demo/domain/repository/CategoryRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.demo.domain.repository
-
-import com.example.demo.domain.model.Category
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface CategoryRepository : JpaRepository<Category, String>
