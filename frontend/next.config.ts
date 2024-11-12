319ea416186a45f4d7b1f3092b2ea121b40345c7
Koji Suehiro <tiripeppa@gmail.com>
2024-11-12 22:08:18 +0900

    nextjsでprerenderingエラーが出る回避

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true,
+    pageExtensions: [
+        "page.tsx",
+        "page.ts",
+        // FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
+        // Instead, use `not-found.ts` as a workaround
+        // "ts" is required to resolve `not-found.ts`
+        // https://github.com/vercel/next.js/issues/65447
+        "ts"
+    ],
+    webpack: (config, { isServer }) => {
+        config.cache = false; // キャッシュを無効化
+
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                fs: false,
+            };
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
