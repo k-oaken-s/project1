6db7e587214697b494c1ba65395f6f47f25c9133
Koji Suehiro <tiripeppa@gmail.com>
2024-11-14 22:49:20 +0900

    責務が明確になるように名称変更

--- backend/src/main/kotlin/tierMaker/infrustructure/PublicFileStorageRepository.kt
+++ backend/src/main/kotlin/tierMaker/infrustructure/PublicFileStorageRepository.kt
@@ -4,7 +4,7 @@ import java.time.Instant
 import org.springframework.stereotype.Service
 
 @Service
-class FileStorageService {
+class PublicFileStorageRepository {
 
     private val baseDir = "public"
 
