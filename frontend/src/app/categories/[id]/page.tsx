ac551a5e5edbfcdd9a28758c0f852f41a789a1ab
Koji Suehiro <tiripeppa@gmail.com>
2024-11-29 18:07:50 +0900

    ユーザーTier:カテゴリ画像表示追加

--- frontend/src/app/categories/[id]/page.tsx
+++ frontend/src/app/categories/[id]/page.tsx
@@ -1,17 +1,19 @@
 'use client';
 
 import TierCreationScreen from '@/app/categories/[id]/TierCreationScreen/TierCreationScreen';
-import { Category } from '@/types/Category';
-import { Item } from '@/types/Item';
-import { getApiBaseUrl } from '@/utils/getApiBaseUrl';
-import { useParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import {Category} from '@/types/Category';
+import {Item} from '@/types/Item';
+import {getApiBaseUrl} from '@/utils/getApiBaseUrl';
+import {useParams} from 'next/navigation';
+import {useEffect, useState} from 'react';
 
 const HomePage = () => {
     const params = useParams();
     const categoryId = Array.isArray(params?.id) ? params.id[0] : params?.id;
     const [items, setItems] = useState<Item[]>([]);
     const [loading, setLoading] = useState(true);
+    const [categoryName, setCategoryName] = useState<string>('');
+    const [categoryImageUrl, setCategoryImageUrl] = useState<string>('');
 
     useEffect(() => {
         // アイテムをバックエンドから取得
@@ -23,6 +25,8 @@ const HomePage = () => {
                 }
                 const category: Category = await response.json();
                 setItems(category.items);
+                setCategoryName(category.name);
+                setCategoryImageUrl(category.image);
             } catch (error) {
                 console.error('エラー:', error);
             } finally {
@@ -38,7 +42,8 @@ const HomePage = () => {
             {loading ? (
                 <p>データを読み込み中...</p>
             ) : (
-                <TierCreationScreen items={items} categoryId={String(categoryId)} />
+                <TierCreationScreen items={items} categoryId={String(categoryId)} categoryName={categoryName}
+                                    categoryImageUrl={categoryImageUrl}/>
             )}
         </div>
     );
--- frontend/src/utils/getApiBaseUrl.tsx
+++ frontend/src/utils/getApiBaseUrl.tsx
@@ -2,5 +2,5 @@ export function getApiBaseUrl(nextPublicApiBaseUrl: boolean = false) {
     if (nextPublicApiBaseUrl) {
         return process.env.API_BASE_URL ?? 'http://backend:8080';
     }
-    return typeof window === 'undefined' ? process.env.API_BASE_URL : process.env.NEXT_PUBLIC_API_BASE_URL!;
+    return process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:8080';
 }
