6ab98975b3def85a9b4a88455b664c45272688ab
Koji Suehiro <tiripeppa@gmail.com>
2024-12-02 22:53:37 +0900

    refactor backend repository interface

--- backend/src/main/kotlin/rankifyHub/category/domain/repository/CategoryRepository.kt
+++ backend/src/main/kotlin/rankifyHub/category/domain/repository/CategoryRepository.kt
@@ -1,13 +1,39 @@
 package rankifyHub.category.domain.repository
 
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 import rankifyHub.category.domain.model.Category
+import java.util.*
 
 /**
- * Category Repository Interface
- *
- * @see JpaRepository
- * @see Category
+ * ドメイン層のCategoryリポジトリインターフェース
  */
-@Repository interface CategoryRepository : JpaRepository<Category, String>
+interface CategoryRepository {
+    /**
+     * すべてのカテゴリーを取得します。
+     *
+     * @return 登録されているカテゴリーのリスト
+     */
+    fun findAll(): List<Category>
+
+    /**
+     * 指定されたIDのカテゴリーを取得します。
+     *
+     * @param id カテゴリーのID
+     * @return 指定されたIDのカテゴリー（Optional）
+     */
+    fun findById(id: String): Optional<Category>
+
+    /**
+     * カテゴリーを保存します。
+     *
+     * @param category 保存するカテゴリー
+     * @return 保存されたカテゴリー
+     */
+    fun save(category: Category): Category
+
+    /**
+     * 指定されたIDのカテゴリーを削除します。
+     *
+     * @param id 削除するカテゴリーのID
+     */
+    fun deleteById(id: String)
+}
--- backend/src/main/kotlin/rankifyHub/category/domain/repository/ItemRepository.kt
+++ backend/src/main/kotlin/rankifyHub/category/domain/repository/ItemRepository.kt
@@ -1,13 +1,22 @@
 package rankifyHub.category.domain.repository
 
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 import rankifyHub.category.domain.model.Item
 
 /**
- * Item Repository Interface
- *
- * @see JpaRepository
- * @see Category
+ * ドメイン層のItemリポジトリインターフェース
  */
-@Repository internal interface ItemRepository : JpaRepository<Item, String> {}
+interface ItemRepository {
+    /**
+     * 最近追加されたアイテムを取得
+     * @param limit 取得する件数
+     * @return 最近追加されたアイテムのリスト
+     */
+    fun findLatest(limit: Int): List<Item>
+
+    /**
+     * 特定の条件に基づいたアイテムを取得
+     * @param condition 条件を表す文字列
+     * @return 条件に合致するアイテムのリスト
+     */
+    fun findByCustomCondition(condition: String): List<Item>
+}
