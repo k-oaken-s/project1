37fff1177f2bd9f902a0f6ace6f3359c098938bc
Koji Suehiro <tiripeppa@gmail.com>
2024-12-03 21:50:30 +0900

    user tier:infrastructure layer

--- backend/src/main/kotlin/rankifyHub/userTier/infrustructure/repository/UserTierJpaRepository.kt
+++ backend/src/main/kotlin/rankifyHub/userTier/infrustructure/repository/UserTierJpaRepository.kt
@@ -1,12 +1,9 @@
 package rankifyHub.userTier.infrastructure.repository
 
+import java.util.*
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import rankifyHub.userTier.domain.model.UserTier
-import java.util.*
 
-/**
- * Spring Data JPAによる基本的なCRUD操作用のリポジトリ
- */
-@Repository
-interface UserTierJpaRepository : JpaRepository<UserTier, UUID>
+/** Spring Data JPAによる基本的なCRUD操作用のリポジトリ */
+@Repository interface UserTierJpaRepository : JpaRepository<UserTier, UUID>
