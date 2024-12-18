810558cf01a938376a9411cff5a4455b8e5febc9
Koji Suehiro <tiripeppa@gmail.com>
2024-12-18 23:38:45 +0900

    fix:CI/CD pipeline workflow

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket         = "koakens-terraform-state-bucket"
-        key            = "ecs/terraform.tfstate"
-        region         = "ap-northeast-1"
-        dynamodb_table = "your-lock-table"
-    }
-}
-
